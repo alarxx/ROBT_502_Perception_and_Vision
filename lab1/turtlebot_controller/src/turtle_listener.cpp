b087991d1761347dd5d81c0433fb15283f2d49cc
Alar Akilbekov <alar.akilbekov@gmail.com>
2026-02-05 00:47:11 +0500

    refactor: lab 1 exercise 4

--- lab1/turtlebot_controller/src/turtle_listener.cpp
+++ lab1/turtlebot_controller/src/turtle_listener.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "turtlesim/Pose.h"
 #include "geometry_msgs/Twist.h"
+#include "turtlesim/Spawn.h"
 
 ros::Publisher pub;
 
@@ -18,6 +19,23 @@ int main (int argc, char **argv){
 	ros::init(argc, argv, "turtlebot_subscriber");
 	ros::NodeHandle nh;
 
+	// ---- Exercise 4 ----
+	ros::service::waitForService("/spawn");
+	ros::ServiceClient client1 = nh.serviceClient<turtlesim::Spawn>("/spawn");
+
+	turtlesim::Spawn srv1;
+	srv1.request.x = 1.0;
+	srv1.request.y = 5.0;
+	srv1.request.theta = 0.0;
+	srv1.request.name = "Turtle_Alar";
+
+	if (client1.call(srv1)) {
+		ROS_INFO("Spawned: %s", srv1.response.name.c_str());
+	} else {
+		ROS_ERROR("Failed to call /spawn");
+	}
+
+	// ---- Exercise 3 ----
 	pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
 
 	// Define the subscriber to turtle's position

882b75a4980363601f53f7b15c40d6129ac598c2
Alar Akilbekov <alar.akilbekov@gmail.com>
2026-02-05 00:38:57 +0500

    refactor: lab 1 exercise 3

--- lab1/turtlebot_controller/src/turtle_listener.cpp
+++ lab1/turtlebot_controller/src/turtle_listener.cpp
@@ -1,21 +1,28 @@
 #include "ros/ros.h"
 #include "turtlesim/Pose.h"
+#include "geometry_msgs/Twist.h"
 
-void turtleCallback(const turtlesim::Pose::ConstPtr& msg)
-{
-	ROS_INFO("Turtle subscriber@[%f, %f, %f]",
-	msg->x, msg->y, msg->theta);
+ros::Publisher pub;
+
+void turtleCallback(const turtlesim::Pose::ConstPtr& msg){
+	ROS_INFO("Turtle subscriber@[%f, %f, %f]", msg->x, msg->y, msg->theta);
+	geometry_msgs::Twist my_vel;
+	my_vel.linear.x = 1.0;
+	my_vel.angular.z = 1.0;
+	pub.publish(my_vel);
 }
 
 
-int main (int argc, char **argv)
-{
-	// Initialize the node, setup the NodeHandle for handling the communication with the ROS
-	//system
+int main (int argc, char **argv){
+	// Initialize the node, setup the NodeHandle for handling the communication with the ROS system
 	ros::init(argc, argv, "turtlebot_subscriber");
 	ros::NodeHandle nh;
+
+	pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+
 	// Define the subscriber to turtle's position
-	ros::Subscriber sub = nh.subscribe("turtle1/pose", 1,turtleCallback);
+	ros::Subscriber sub = nh.subscribe("turtle1/pose", 1, turtleCallback);
+
 	ros::spin();
 	return 0;
 }
